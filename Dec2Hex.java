0345646bc3642c15fd325c470d9eddc7234110f2
dhusar200 <dhusar200@caledonian.ac.uk>
2019-11-08 16:15:13 +0000

    Invalid with no input

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,7 +7,10 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-        if (args.length > 0) 
+        if (args.length == 0) {
+            System.err.println("There was no input, please try again.");
+        }
+        else if (args.length > 0)
         {
             try 
             {
@@ -18,22 +21,21 @@ class Dec2Hex
                 System.err.println("Argument" + args[0] + " must be an integer.");
                 System.exit(1);
             }
-        }
 
-    	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-         int rem, num;
-	num = Arg1;
-	String hexadecimal=""; 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-        
-        while(num != 0)
-        {
-	    rem=num%16;
-	    hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
-        
-        System.out.println("Hexadecimal representation is : " + hexadecimal);
+            char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+            int rem, num;
+            num = Arg1;
+            String hexadecimal="";
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
+            while(num != 0)
+            {
+                rem=num%16;
+                hexadecimal= ch[rem] + hexadecimal;
+                num= num/16;
+            }
+
+            System.out.println("Hexadecimal representation is : " + hexadecimal);
+        }
     }
 }
