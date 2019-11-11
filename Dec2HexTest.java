ad29ba60d405319e10c7612f90274ec830ff6768
dhusar200 <dhusar200@caledonian.ac.uk>
2019-11-11 18:14:57 +0000

    unittest

---
+++ Dec2HexTest.java
@@ -0,0 +1,36 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Dec2HexTest {
+
+    static Dec2Hex tester;
+
+    @BeforeClass
+    public static void testSetup() {
+        tester = new Dec2Hex();
+    }
+
+    @Test
+        public void positiveNumber(){
+        String[] array = new String[]{"100"};
+        tester.main(array);
+    }
+
+    @Test
+    public void negativeNumber(){
+        String[] array = {"-200"};
+        tester.main(array);
+    }
+
+    @Test
+    public void emptyinput(){
+        String[] array = new String[]{""};
+        tester.main(array);
+    }
+
+    @Test
+    public void stringInput(){
+        String[] array = new String[]{" Test"};
+        tester.main(array);
+    }
+}
